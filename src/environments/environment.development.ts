c8bd1728d4964e34611fc2bc7c9bde95e4fd2ae6
nhuamani <nhuamani.c7@gmail.com>
2024-11-20 22:18:05 -0500

    fix: deploy

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,7 @@
 export const environment = {
-    HOST: 'http://localhost:8080/api/v1',  
-    paypalReturnUrl: 'http://localhost:4200/pages/reservation/form'
+    // HOST: 'http://localhost:8080/api/v1',  
+    // paypalReturnUrl: 'http://localhost:4200/pages/reservation/form'
+
+    HOST: 'https://restaurantreservationapi.onrender.com/api/v1',
+    paypalReturnUrl: 'https://reservationrestaurant.netlify.app/pages/reservation/form'
 };
